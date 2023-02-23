2a3d0aa3d907836efe92f8fedf21280607744a6f
Mikołaj Brzoskowski <mikolaj.brzoskowski2000@gmail.com>
2023-02-23 17:52:42 +0100

    added History UI

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,8 +1,50 @@
 import React from 'react'
+import { Container, Row, Col, Card, Button, Placeholder } from 'react-bootstrap'
+import { lorem_ipsum } from '../lorem_ipsum'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Container>
+      <Row>
+          <p className="text-center">Welcome to MapApp. Here you can check the route to the destination along with the cost of the trip.</p>
+          <p className="text-center">{lorem_ipsum}</p>
+      </Row>
+      <Row id="history">
+          <h1 className="text-center">History</h1>
+      </Row>
+      <Row>
+        <Col className="d-flex justify-content-center m-1">
+          <Card style={{ width: '18rem' }}>
+            <Card.Body>
+              <Card.Title>From: Starting address</Card.Title>
+              <Card.Title>To: Ending address</Card.Title>
+              <Card.Text>
+                Cost: Here will go cost of the trip
+              </Card.Text>
+              <Button variant="primary">Go to the details with route</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        {Array(4).fill().map(() => (
+        <Col className="d-flex justify-content-center m-1">
+          <Card style={{ width: '18rem' }}>
+            <Card.Body>
+              <Placeholder as={Card.Title} animation="glow">
+                <Placeholder xs={3} /> <Placeholder xs={6} />
+              </Placeholder>
+              <Placeholder as={Card.Title} animation="glow">
+                <Placeholder xs={2} /> <Placeholder xs={6} />
+              </Placeholder>
+              <Placeholder as={Card.Text} animation="glow">
+                <Placeholder xs={3} /> <Placeholder xs={8} />
+              </Placeholder>
+              <Placeholder.Button variant="primary" xs={10} />
+            </Card.Body>
+          </Card>
+        </Col>
+        ))}
+      </Row>
+    </Container>
   )
 }
 
